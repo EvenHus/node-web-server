381c88003aa804ae679dd4bb0f454b31e4f60cc8
Even Hus <even.hus.94@gmail.com>
2018-05-01 14:21:14 +0200

    Added projects page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'My projects'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         error: 'Some error here',
